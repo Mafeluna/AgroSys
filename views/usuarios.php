e239e061ff75df48d803fc1ef46f94a8def290ed
Nicolás Moreno <nmp0617@gmail.com>
2025-06-27 12:41:41 -0500

    barra de busqueda usuarios lista

--- views/usuarios.php
+++ views/usuarios.php
@@ -78,6 +78,19 @@
             Generar Excel
           </button>
           </div>
+          <div class="w-2/3">
+          <form action="" class="flex gap-10" method="post">
+            <input
+              name = "documento"
+              type="text"
+              placeholder="Buscar por Documento..."
+              class="w-3/4 pl-10 pr-4 py-2 rounded-2xl shadow-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-all duration-200"
+            />
+            <button class="bg-blue-300 duration-150 hover:!border-b-2 text-blue-950 rounded-xl drop-shadow-lg group flex items-center border-2 border-b-4 border-blue-950 cursor-pointer p-3 font-semibold hover:bg-yellow-400x">
+              <ion-icon name="search-outline"></ion-icon>
+            </button>
+          </form>
+          </div>
         </header>
         <article class="w-full p-5">
           <section class="w-full activesection seccion flex-col justify-center items-center" id="consultaUser">
